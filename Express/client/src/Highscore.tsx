a797fa6a70e1bcd80131856fc755667710a7bfc4
Andreas Blaabjerg <Andreas.blaabjerg1@hotmail.com>
2019-12-16 23:10:24 +0100

    highscore.tsx begun

---
+++ Express/client/src/Highscore.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { IProps } from './Grid';
+//import { IState } from './Game'; // might be needed for highscore seeing as score is implemented in game.
+
+
+
+class HighscoreForm extends React.Component {
+
+    
+}
